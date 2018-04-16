cb7351f7b3081d8be648c1d08a04d24a3f046cf0
anorsich <an.orsich@gmail.com>
2018-04-16 16:36:15 +0300

    [-] Fix linting issues

--- src/service/src/storage/index.js
+++ src/service/src/storage/index.js
@@ -2,11 +2,11 @@ const config = require('config');
 
 // This is the way to implement more storages for files in the future
 const getStorage = (type) => {
-  switch(type) {
+  switch (type) {
     default:
       return require('./mongo.storage'); // eslint-disable-line
   }
-}
+};
 
 const storage = getStorage(config.storage.type);
 
