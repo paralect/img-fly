4f8f9a38f9c1f43e86d21f0adaacd48f493270ff
anorsich <an.orsich@gmail.com>
2018-04-16 13:08:55 +0300

    [*] add colors to the new-release.sh

--- bin/new-release.sh
+++ bin/new-release.sh
@@ -1,3 +1,10 @@
 #!/bin/sh
-git tag $1
-git push origin $2
+Green='\033[0;32m' 
+BIGreen='\033[1;92m'
+Color_Off='\033[0m'
+echo $BIGreen"Latest releases: $Green"
+git tag -l --sort -refname | head -n 3
+echo $Color_Off
+echo $1
+echo $2
+git tag -a "$1" -m "$2" && git push origin $1
